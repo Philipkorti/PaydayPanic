41df437a98c2dccdf33b53dc6e4e48cee979f5f1
PhilipKorti <philip.korthauer@gmx.at>
2023-04-03 19:09:16 +0200

    Create MVVM

--- PayDay/PayDay/App.xaml.cs
+++ PayDay/PayDay/App.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Microsoft.Practices.Prism.Events;
+using PayDay.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +16,30 @@ namespace PayDay
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Raises the <see cref="System.Windows.Application.Startup"/> event.
+        /// </summary>
+        /// <param name="e">A <see cref="System.Windows.StartupEventArgs"/>the event data.</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Get the reference to the same name as the current one
+            Process thisproc = Process.GetCurrentProcess();
+
+            // Check how many processes have the same name as the current one
+            if(Process.GetProcessesByName(thisproc.ProcessName).Length > 1)
+            {
+                MessageBox.Show("Application is already running!");
+                Application.Current.Shutdown();
+                return;
+            }
+
+            // Init event aggregator and services
+            IEventAggregator eventAggregator = new EventAggregator();
+            // Init view and viewmodel
+            MainWindow window = new MainWindow();
+            MainViewModel viewModel = new MainViewModel();
+            window.DataContext = viewModel;
+            window.Show();
+        }
     }
 }
