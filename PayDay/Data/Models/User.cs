9794473db2bc55e8dd3fb7296eb8c7fc6a002ef5
PhilipKorti <philip.korthauer@gmx.at>
2023-04-04 16:01:00 +0200

    DB Create Tabel with EntityFramework

---
+++ PayDay/Data/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public int Elo { get; set; } = 0;
+
+        public int highscore { get; set; } = 0;
+
+        public int Goldscore { get; set; } = 0;
+
+        public ICollection<Items> Items { get; set; } = null;
+    }
+}
